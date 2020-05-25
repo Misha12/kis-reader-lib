13e29258bd6e59b000042c9e4e2d78b3f85e7428
Michal Kotoun <xkotou04@stud.fit.vutbr.cz>
2020-05-26 01:41:42 +0200

    Fix fix-es6-imports

--- fix-es6-imports.sh
+++ fix-es6-imports.sh
@@ -1,1 +1,1 @@
-find distES6 -type f -name '*.js' -print0 | xargs -0 sed --in-place -E "s/from [\"\']([^\"\']+[^.][^j][^s])[\"\'];$/from \"\\1.js\";/g"
+find distES6 -type f -name '*.js' -print0 | xargs -0 perl -pi -e "s/from [\"\']([^\"\']+)"'(?<!\.js)'"[\"\'];/from \"\\1.js\";/g"
