4ede4fb2afa062f9ac9a2356770f48d618a8b3ba
Michal Kotoun <xkotou04@stud.fit.vutbr.cz>
2020-05-26 00:28:06 +0200

    Fix + extend readOneCard(); Add testReader() into lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -3,12 +3,15 @@ import { KisReaderClient } from "./reader";
 export { KisReaderClient } from "./reader";
 export { WrapperReader } from "./wrapperReader";
 
-export const readOneCard = (readerUri: string, onData: (data: string) => void, onError: (err: any) => void) : ((any) => void) => {
-    if (!(readerUri.startsWith("ws:// ") || readerUri.startsWith("wss:// ")))
-        readerUri = "wss:// " + readerUri;
+// takes URI, onData, onError, onConnect(optional) callbacks, returns disconnect lambda
+export const readOneCard = (readerUri: string, onData: (data: string) => void, onError: (err: any) => void, onConnect?: () => void) : (() => void) => {
+    if (!(readerUri.startsWith("ws://") || readerUri.startsWith("wss://")))
+        readerUri = "wss://" + readerUri;
     let client = new KisReaderClient(readerUri);
     client.disconnectedEvent.once(onError);
     client.connectedEvent.once(reader => reader.modeSingleRead());
+    if (onConnect)
+        client.connectedEvent.once(onConnect);
     client.cardReadEvent.once(ev => {
         client.disconnectedEvent.offOnce(onError);
         client.disconnect();
@@ -20,3 +23,13 @@ export const readOneCard = (readerUri: string, onData: (data: string) => void, o
         client.disconnect();
     }
 };
+
+// for testing the availability of the reader - will likely fail on alredy-connected reader
+// takes URI, onResult callbacks, returns nothing
+export const testReader = (readerUri: string, onResult: (success: boolean) => void) : void => {
+    let client = new KisReaderClient(readerUri);
+    client.connectedEvent.once(() => onResult(true));
+    client.connectedEvent.once(() => client.disconnect());
+    client.errorEvent.once(() => onResult(false));
+    client.connect();
+}
